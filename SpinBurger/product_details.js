52b399a60238be64a5351b61271c9ffe193fd94f
soreilly6 <88727212+soreilly6@users.noreply.github.com>
2021-12-13 16:58:33 +0000

    Create product_details.js

---
+++ SpinBurger/product_details.js
@@ -0,0 +1,99 @@
+var data = {
+  
+  product: {
+    "BURGER_4" : {
+      productName: "Veggie Burger",
+      description: "Packed with flavour, these vegan bean burgers are healthy and seriously satisfying",
+      price: 7.99,
+      currency: "USD",
+      formattedPrice: "$7.99",
+      primaryCategory: "BURGER",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2FScreenshot%202021-11-08%20at%2012.39.45.png?v=1636375225042" 
+    },
+    "BURGER_2" : {
+      productName: "Cheese Burger",
+      description: "Classic juicy meaty beef burger topped with delicious melting chedder cheese",
+      price: 7.00,
+      currency: "USD",
+      formattedPrice: "$7.00",
+      primaryCategory: "BURGER",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2FScreenshot%202021-11-08%20at%2012.41.06.png?v=1636375284011"
+    },
+    "BURGER_1" : {
+      productName: "Classic Burger",
+      description: "Classic juicy meaty beef burger, with onions, tomatoes and fresh lettuce",
+      price: 6.50,
+      currency: "USD",
+      formattedPrice: "$6.50",
+      primaryCategory: "BURGER",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2Fturkey-burger-12.jpeg?v=1636046678187"
+    },
+    "BURGER_3" : {
+      productName: "Chicken Burger",
+      description: "Crispy seasoned chicken breast,  piled onto soft rolls with onion, avocado, lettuce, tomato and garlic mayo.",
+      price: 7.00,
+      currency: "USD",
+      formattedPrice: "$7.00",
+      primaryCategory: "BURGER",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2FScreenshot%202021-11-08%20at%2012.42.02.png?v=1636375340639"
+    },
+    "SIDE_1" : {
+      productName: "Fries",
+      description: "prepared by cutting the potato into even strips, then drying and frying it in a deep fryer",
+      price: 2.35,
+      currency: "USD",
+      formattedPrice: "$2.35",
+      primaryCategory: "SIDE",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2FFrench-fries-b9e3e0c.jpeg?v=1636373214284" 
+    },
+    "SIDE_2" : {
+      productName: "Sweet Potato Fries",
+      description: "A variation of French fries using sweet potato instead of potato. Served lightly salted",
+      price: 2.99,
+      currency: "USD",
+      formattedPrice: "$2.99",
+      primaryCategory: "SIDE",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2Fbaked-sweet-potato-fries-5.jpeg?v=1636373338736"
+    },
+    "SIDE_3" : {
+      productName: "Onion Rings",
+      description: "Onion dipped in batter or bread crumbs and then deep fried",
+      price: 2.50,
+      currency: "USD",
+      formattedPrice: "$2.50",
+      primaryCategory: "SIDE",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2Fhot-and-tasty-onion-rings-2.jpeg?v=1636373650148"
+    },
+    "DRINK_1" : {
+      productName: "Coca Cola",
+      description: "A 330ml can of Coca Cola",
+      price: 0.95,
+      currency: "USD",
+      formattedPrice: "$0.95",
+      primaryCategory: "DRINK",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2FAU00099-X.jpeg?v=1636374094065" 
+    },
+    "DRINK_2" : {
+      productName: "Milkshake",
+      description: "This is the ultimate Nutella milkshake recipe. It's creamy, rich and nutty",
+      price: 2.50,
+      currency: "USD",
+      formattedPrice: "$2.50",
+      primaryCategory: "DRINK",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2FUltimate-Nutella-Milkshake-square.jpeg?v=1636374284631"
+    },
+    "DRINK_3" : {
+      productName: "Water",
+      description: "A 500ml bottle of spring water",
+      price: 1.05,
+      currency: "USD",
+      formattedPrice: "$1.05",
+      primaryCategory: "DRINK",
+      imageURL: "https://cdn.glitch.me/ed0af3cb-3d61-45d0-8687-6868bf4a89a8%2Fscrew-top-gettyimages-85210965.jpeg?v=1636374194027"
+    }
+  },
+  cart: {
+    //dictionary --> itemId : productId
+  },
+  cartTotal: 0
+};
